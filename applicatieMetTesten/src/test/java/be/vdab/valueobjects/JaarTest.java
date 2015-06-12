b0ad86413eb38987a2d9087994471690b72609c6
JohanDullaert <johan.dullaert@vdab.be>
2015-06-12 11:49:03 +0200

    first commit of JUnit course example

---
+++ applicatieMetTesten/src/test/java/be/vdab/valueobjects/JaarTest.java
@@ -0,0 +1,31 @@
+package be.vdab.valueobjects;
+
+import static org.junit.Assert.*;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class JaarTest {
+
+	@Test
+	public void eenJaarDeelbaarDoor400IsEenSchrikkeljaar() {
+		Jaar jaar = new Jaar(2000);
+		assertTrue(jaar.isSchrikkeljaar());
+	}
+
+	@Test
+	public void eenJaarDeelbaarDoor100IsGeenSchrikkeljaar() {
+		assertFalse(new Jaar(1900).isSchrikkeljaar());
+	}
+	
+	@Test
+	public void eenJaarDeelbaarDoor4IsEenSchrikkeljaar() {
+		assertTrue(new Jaar(2012).isSchrikkeljaar());
+	}
+	
+	@Test
+	public void eenJaarNietDeelbaarDoor4IsGeenSchrikkeljaar() {
+		assertFalse(new Jaar(2015).isSchrikkeljaar());
+	}
+	
+}
---
+++ applicatieMetTesten/src/test/java/be/vdab/valueobjects/RekeningnummerTest.java
@@ -0,0 +1,11 @@
+package be.vdab.valueobjects;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class RekeningnummerTest {
+
+	
+
+}
